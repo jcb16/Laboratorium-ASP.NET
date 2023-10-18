8b8b94a1b73500ed82cbf10f96030c395f6c4236
Jakub Kolber <jakub.kolber@microsoft.wsei.edu.pl>
2023-10-18 15:46:55 +0200

    zad3

---
+++ Laboratorium 3 - App/Controllers/ContactController.cs
@@ -0,0 +1,72 @@
+﻿using Laboratorium_3___App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3___App.Controllers
+{
+    public class ContactController : Controller
+    {
+        //lista kontaktów
+        static Dictionary<int,Contact> _contacts = new Dictionary<int,Contact>();
+        static int id = 1;
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Contact model) { 
+            if(ModelState.IsValid)
+            {
+                // dodaj model do bazy lub kolekcji 
+                model.ID = id++;
+                _contacts.Add(model.ID, model);
+                return RedirectToAction("Index");
+            }
+            return View(); 
+        }
+
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if(ModelState.IsValid)
+            {
+                _contacts[model.ID] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet] 
+        public IActionResult Delete(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact model)
+        {
+            _contacts.Remove(model.ID);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id) 
+        {
+            return View(_contacts[id]);
+        }
+    }
+}
---
+++ Laboratorium 3 - App/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3___App.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int ID { get; set; }
+        
+        [Required(ErrorMessage ="Musisz podać imię!")]
+        [StringLength(maximumLength:50, ErrorMessage ="Imię zbyt długie, maksymalnie 50 znaków!")]
+        public string Name { get; set; }
+        
+        [EmailAddress(ErrorMessage = "Musisz podać poprawny email (brak znaku @)")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage ="Numer telefonu powinien zawierać cyfry")]
+        public string Phone { get; set; }
+        
+        public DateTime? Birth {  get; set; }
+
+    }
+}
